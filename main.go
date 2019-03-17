05e2775ff73139df4b4fbf0cebd71cfe0cc0e8ae
Takahiro Oshima <tarotora51@gmail.com>
2019-03-18 01:55:11 +0900

    追加

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// パスフレーズのある鍵を利用するパターンでは下記で行けると持ったけど無理だった。別途調査が必要
 	//passPhrase := []byte("test")
 	//privateKey, err := ssh.ParsePrivateKeyWithPassphrase(privateKeyBytes, passPhrase)
 
@@ -39,19 +40,45 @@ func main() {
 	log.Println("Listening on 2222...")
 
 	for {
+
+		// 先にTCPコネクションができていないと接続できない理由はRFCに書いて有りそう。
 		tcpConn, err := listener.Accept()
 		if err != nil {
 			log.Fatalf("Failed to accept on 2222 (%s)", err)
 		}
 
+		// RFCに基づいて接続確立方法が正しいのか確認しておく。
 		sshConn, chans, reqs, err := ssh.NewServerConn(tcpConn, serverConfig)
-		if err != nil {
-			log.Fatalf("Failed to handshake (%s)", err)
-		}
-		log.Printf("New SSH connection from %s (%s)", sshConn.RemoteAddr(), sshConn.ClientVersion())
 
-		go ssh.DiscardRequests(reqs)
-		go handleChannels(chans)
+		// sshConnで接続元IPアドレスが特定できるのでアクセスもとをtomlファイルでユーザごとに制御することは可能
+		// 接続先はsshConnでは特定することができないので、事前にユーザに紐づけをしておくとかにする以外は方法がなさそう。
+		// ポートフォワーディングみたいなことをすれば、いいのかな。（鍵認証が使えなくなる気がする）
+		log.Println("User:", sshConn.User())
+		log.Println("Remote address:", sshConn.RemoteAddr())
+		log.Println("Local address:", sshConn.LocalAddr())
+
+		// User Check
+		// tomlファイルからアクセスリストを取り出す方式にすれば問題なさそう。
+		if sshConn.User() == "mockoro" {
+			log.Println("User check OK")
+			if err != nil {
+				log.Fatalf("Failed to handshake (%s)", err)
+			}
+			log.Printf("New SSH connection from %s (%s)", sshConn.RemoteAddr(), sshConn.ClientVersion())
+
+			go ssh.DiscardRequests(reqs)
+			go handleChannels(chans)
+		} else {
+			log.Println("User check NG")
+			err := sshConn.Close()
+			if err != nil {
+				log.Println("SSH Connection Close Failed: ", err)
+			}
+			err = tcpConn.Close()
+			if err != nil {
+				log.Println("TCP Connection Close Failed: ", err)
+			}
+		}
 	}
 }
 
